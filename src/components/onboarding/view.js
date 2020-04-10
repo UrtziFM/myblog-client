a7b74a2776e98c848bab500309ed6704046952da
UrtziFM <urtzi.arana13@gmail.com>
2020-04-10 19:16:01 +0200

    Onboarding is finished, now start with login and home

--- src/components/onboarding/view.js
+++ src/components/onboarding/view.js
@@ -2,18 +2,27 @@ import React from 'react';
 
 import './styles.css';
 
+import OnboardingModal from '../onboardingmodal/view';
+
 function OnboardingCard() {
-  
+  const [modalShow, setModalShow] = React.useState(false);
+
   return (
-    <div className="BlogHello" /*onClick={() => setModalShow(true)}*/>
+    <div className="BlogHello">
       <div className="BlogLogo" >
         <img className="logo" src="assets/bloglogo.jpg" alt="imagen Logo" />
       </div>
       <p className="BlogName">"Urtzi's Green Thoughts"</p>
-      <p className="welcome">Welcome</p>
-      <p>
+      <p className="welcome">Pulsa mi Eguzkilore</p>
+      <p onClick={() => setModalShow(true)}>
         <img className="eguzki" src="assets/eguzki.jpeg" alt="imagen Eguzkilore" />
       </p>
+    
+      <OnboardingModal
+      show={modalShow}
+      onHide={() => setModalShow(false)}
+      />
+
     </div>
   );
 }
---
+++ src/components/onboardingmodal/view.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import Modal from 'react-bootstrap/Modal';
+import { useHistory } from 'react-router-dom';
+
+import './styles.css';
+
+function OnboardingModal(props) {
+    const [modalShow, setModalShow] = React.useState(false);  
+    const modalClass = modalShow ? '' : 'ModalBox--hidden'
+    const [selected, setSelected] = React.useState(false);
+    const history = useHistory();
+
+    return (
+    <div className={`ModalBox ${modalClass}`} >
+      <Modal 
+        {...props}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Body>
+                <div className="Modal1_Text">Acceso al Blog</div>
+                <div className="Modal_options">
+                    <div className={selected==="option0" ? "option0_1" : 'option0'} onClick={() => setSelected("option0")}>
+                    <img src="assets/lector.jpeg" alt="point0"/><p>Lector</p></div>
+                    <div className={selected==="option1" ? "option1_1" : 'option1'} onClick={() => setSelected("option1")}>
+                    <img  src="assets/escritor.png" alt="point1"/><p>Urtzi</p></div>
+                </div>
+                {selected ? 
+                <div className="Modal_apply_options">
+                <div className="Modal_apply_clean" onClick={()=> history.push('/home')}>Acceso</div>
+                <div className="Modal_apply_accept"onClick={()=> history.push('/login')}>Acceso</div>
+                </div>
+                :
+                <div className="Modal_apply">¿Quien eres?</div>
+                }        
+        </Modal.Body>
+      </Modal>
+    </div>
+    );
+  }
+
+  export default OnboardingModal;
