1ce8a84bf554076b8b43f8a7ad4a0e64b937792d
UrtziFM <urtzi.arana13@gmail.com>
2020-04-10 14:14:48 +0200

    Onboarding Card is done

---
+++ src/components/onboarding/view.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import './styles.css';
+
+function OnboardingCard() {
+  
+  return (
+    <div className="BlogHello" /*onClick={() => setModalShow(true)}*/>
+      <div className="BlogLogo" >
+        <img className="logo" src="assets/bloglogo.jpg" alt="imagen Logo" />
+      </div>
+      <p className="BlogName">Urtzi's Green Thoughts</p>
+      <p className="welcome">Welcome</p>
+      <p>
+        <img className="eguzki" src="assets/eguzki.jpeg" alt="imagen Eguzkilore" />
+      </p>
+    </div>
+  );
+}
+
+export default OnboardingCard;
