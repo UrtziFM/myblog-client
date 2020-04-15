9c60736ef8732bdf9aaefdcffca28c9967865f19
UrtziFM <urtzi.arana13@gmail.com>
2020-04-15 23:34:37 +0200

    Home container lastpost card

--- src/components/lastpost/view.js
+++ src/components/lastpost/view.js
@@ -2,11 +2,17 @@ import React from 'react';
 
 import './styles.css';
 
-function LastPostCard () {
+const LastPostCard = ({title, picture, description, date, views}) => {
 
     return (
-        <div>
-            <h3>Hellooo</h3>
+        <div className="last-Card">
+            <h3 className="last-Card-Title">{title}Hola</h3>
+            <div clasName="last-Card-Box">
+            <img src="assets/eguzki.jpeg" className="last-Card-Img" alt="foto"></img>
+            <p className="last-content">{description}Holasssndsabfnmbdsbfanmdsbmadbmbma</p>
+            <p clasName="last-date">{date}Holass</p>
+            <p className="last-views">{views}Holasssssssssssssssss</p>
+            </div>
         </div>
     )
 }
