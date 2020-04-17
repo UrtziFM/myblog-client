da05138c0cdfdabf3754ecf2ed85f0313d9f0a8b
UrtziFM <urtzi.arana13@gmail.com>
2020-04-17 21:18:29 +0200

    Home Container is almost finished, I am only need a Carousel for List Post

--- src/components/lastpost/view.js
+++ src/components/lastpost/view.js
@@ -6,12 +6,11 @@ const LastPostCard = ({title, picture, description, date, views}) => {
 
     return (
         <div className="last-Card">
-            <h3 className="last-Card-Title">{title}Hola</h3>
-            <div clasName="last-Card-Box">
-            <img src="assets/eguzki.jpeg" className="last-Card-Img" alt="foto"></img>
+            <h3 className="last-Card-Title">Último Post: {title}</h3>
             <p className="last-content">{description}Holasssndsabfnmbdsbfanmdsbmadbmbma</p>
-            <p clasName="last-date">{date}Holass</p>
-            <p className="last-views">{views}Holasssssssssssssssss</p>
+            <div className="last-footer">
+            <p className="last-date">Día: {date}13-02-20</p>
+            <p className="last-views">Views: {views}10.000</p>
             </div>
         </div>
     )
--- src/components/postlist/view.js
+++ src/components/postlist/view.js
@@ -1,13 +1,29 @@
 import React from 'react';
+import LastPostCard from '../lastpost';
+import PostList from '../../constants/PostList'
 
 import './styles.css';
 
-function PostListCard () {
+const PostListCard = () => {
 
     return (
         <div>
-            <h3>Hellooo</h3>
-        </div>
+            <h3 className= "PostList-Title"> Lista con todos mis Post</h3>
+        <div>
+         {PostList.map((PostList, i) => {
+           return (
+             <LastPostCard
+               key={i}
+               title={PostList.title}
+               description={PostList.description}
+               date={PostList.date}
+               views={PostList.views}
+             />
+           );
+         })}
+       </div>
+     </div>
+ 
     )
 }
 
