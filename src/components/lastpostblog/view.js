59ea95517dbc8ccebd7b24f35f9268e5d73c60f3
UrtziFM <urtzi.arana13@gmail.com>
2020-04-24 19:02:20 +0200

    lastblog I have to add link scroll

--- src/components/lastpostblog/view.js
+++ src/components/lastpostblog/view.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card'
+import { Link, animateScroll as scroll } from "react-scroll";
 
 
 import Navbar from '../../components/navbar'
@@ -15,9 +16,9 @@ const LastPostBlog = ({title, picture, contentPost, category, date, views}) => {
             <Card.Img variant="top" src="assets/bloglogo.jpg" />
             <h3 className="last-Card-Title-Blog">La Falacia del Desarrollo Sostenible {title}</h3>
             <p className="last-type-Blog">Green Economy{category}</p>
+            <div className="last-line"></div>
             <div className="last-text-Blog">
-                <img className="last-img-Blog" src={picture}></img>
-                <p className="last-main-Blog">hhhhhhh{contentPost}</p>
+                <p className="last-main-Blog">Este es el cuento de nunca acabar, una dia empiezas y otro dia terminas {contentPost}</p>
             </div>
             <div className="last-footer-Blog">
             <p className="last-date-Blog">Día: {date}</p>
