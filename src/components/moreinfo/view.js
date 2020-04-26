752a652ed047d4edc3580feb5515d26725da2d3f
UrtziFM <urtzi.arana13@gmail.com>
2020-04-26 18:07:24 +0200

    each component is finished, now I have to make routes well

--- src/components/moreinfo/view.js
+++ src/components/moreinfo/view.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { MDBCol, MDBContainer, MDBRow, MDBFooter } from "mdbreact";
+import { MDBContainer, MDBFooter } from "mdbreact";
 
 import Navbar from '../../components/navbar'
 
@@ -11,9 +11,17 @@ const MoreInfoCard = () => {
         <Navbar/>
         <div className="moreinfo-Card-Blog">
         <img className="moreinfo-photo" src="assets/urtzi.jpg" />
-        </div>
         <div className="moreinfo-text">
-          <p className="moreinfo-text-detail">Hola bienvenido</p>
+          <p className="moreinfo-text-detail">Urtzi Arana Santamaria</p>
+          <p className="moreinfo-text-detail-3">"Se que mis pensamientos o ideas simplemente son 
+           un momento de lucidez temporal, sin la ejecución o el intento estos pensamientos solo serían 
+           glucosa y oxígeno"</p>
+           <p className="moreinfo-text-detail-2">Yo</p>
+          <p className="moreinfo-text-detail-1">"En realidad no me preocupa que me quieran robar mis ideas, 
+          me preocupa que ell@s no las tengan"</p>
+          <p className="moreinfo-text-detail-2">Nicola Tesla</p>
+          
+        </div>
         </div>
         <MDBFooter color="green" className="font-small pt-4 mt-4">
         <div className="footer-copyright text-center py-3">
